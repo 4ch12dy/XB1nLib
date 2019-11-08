56e1c3249e6cd7bbcd6f80b426703d8302a6ad16
xia0 <939529983@qq.com>
2019-11-08 18:05:29 +0800

    modify XDLOG use printf

--- xia0.h
+++ xia0.h
@@ -5,5 +5,8 @@
 #define XSLOG(log, ...)	printf("[+] " log, ##__VA_ARGS__)
 #define XELOG(log, ...)	fprintf(stderr, "[-] " log, ##__VA_ARGS__);perror(":")
 #define XILOG(log, ...)	printf("[*] " log, ##__VA_ARGS__)
-#define XDLOG(log, ...)	fprintf(stderr,"[D] " log, ##__VA_ARGS__)
+#define XDLOG(log, ...)	printf("[D] " log, ##__VA_ARGS__)
+
+//#define XDLOG(log, ...)	fprintf(stderr,"[D] " log, ##__VA_ARGS__)
+
 
